c613cd366b0ca705a45f6e508dbda0d4fbe5f72b
mrjln <marjolein_tromp@hotmail.com>
2017-11-05 21:35:09 +0100

    add image pirates life

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Hangman from './components/Hangman.js'
 import Addguess from './components/AddGuess.js'
 import Pirateslogo from './images/pirateslogo.jpg'
+import Pirateslife from './images/piratelife.jpg'
 
 
 class App extends Component {
@@ -11,14 +12,19 @@ class App extends Component {
       <div className="App">
           <h1 className="App-title">Welcome to Hangman</h1>
           <img src = {Pirateslogo} />
-
+          <hr />
             <label>
               Take a guess, write a letter and hit `Guess`
             </label>
             <h1>Add a guess </h1>
-            <Addguess />
           <Hangman />
+          <h1>Lifes left </h1>
+          <img class="life" src = {Pirateslife} alt= "life"/>
+          <img class="life" src = {Pirateslife} alt= "life" />
+
       </div>
+
+
     )
   }
 }
